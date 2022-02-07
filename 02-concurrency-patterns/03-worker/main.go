8112965157f9166ac8428b37727da2e14a50307c
Magesh Kuppan <tkmagesh77@gmail.com>
2022-02-07 17:00:26 +0530

    worker - problem statement

---
+++ 02-concurrency-patterns/03-worker/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var names = []string{
+	"steve",
+	"bob",
+	"mary",
+	"therese",
+	"jason",
+	"Magesh",
+	"Ganesh",
+	"Ramesh",
+	"Rajesh",
+	"Suresh",
+}
+
+type NamePrinter struct {
+	name  string
+	delay time.Duration
+}
+
+func (np *NamePrinter) Task() {
+	fmt.Println("Task execution commenced")
+	time.Sleep(np.delay)
+	fmt.Println("Name Printer - Name : ", np.name)
+}
+
+func main() {
+	w := worker.New(5)
+	timerCounter := 1
+	for idx := 0; idx < 2; idx++ {
+		for _, name := range names {
+			np := &NamePrinter{
+				name:  name,
+				delay: time.Duration(timerCounter*100) * time.Microsecond,
+			}
+			timerCounter++
+			w.Run(np)
+		}
+	}
+	fmt.Println("All tasks are assigned")
+	w.Shutdown()
+}
